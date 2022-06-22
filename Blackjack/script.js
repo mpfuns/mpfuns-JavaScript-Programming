f7399619907d0485c3901c11a0752a499e0b668a
mpfuns <monetpaul23@gmail.com>
2022-06-22 14:11:20 -0400

    done 6/22

--- Blackjack/script.js
+++ Blackjack/script.js
@@ -1,11 +1,75 @@
-let firstCard = 4;
-let secondCard= 8;
-
-let sum = firstCard +secondCard;
-if(sum < 21){
-    console.log(`Do you want to draw a new card?🎰`);
-} else if (sum=== 21){
-    console.log(`whoo! You've got Black jack!💸"`)
-} else if ( sum >21){
-    console.log(`You're out of  the game! 😈 `)
+let player= {
+    name: "Per",
+    chips: 200
+    
 }
+let cards=[];
+let sum = 0;
+let hasBlackJack =false
+let isAlive=false;
+let message= ``;
+
+let messageEl=document.getElementById("message-el");
+let sumEl=document.getElementById("sum-el");
+let cardsEl=document.getElementById("cards-el")
+
+let playerEl= document.getElementById("player-el");
+playerEl.textContent= player.name + ": $" + player.chips
+ 
+
+function getRandomCard(){
+       let random=Math.floor(Math.random()*13)+1;
+
+       if (random >10){
+           return 10;
+       } else if (random===1){
+           return 11;
+       } else{
+           return random;
+       }
+ }
+
+
+
+function startGame(){
+    
+    isAlive=true;
+    let firstCard= getRandomCard();
+    let secondCard=getRandomCard();
+    cards=[firstCard,secondCard];
+    sum= firstCard+secondCard;
+    renderGame();
+}
+ function renderGame(){
+    //render out all the we have 
+    cardsEl.textContent= "Cards: " 
+   for(let i=0; i<cards.length;i++){
+       cardsEl.textContent+=cards[i]+ " ";
+   }
+   
+   
+    sumEl.textContent= "Sum: " + sum;
+    
+    if(sum <= 20){
+        message=`Do you want to draw a new card?`;
+    } else if (sum=== 21){
+        message=`You've got Black jack!`;
+        hasBlackJack= true;
+    } else {
+        message=`You're out of  the game!`;
+        isAlive=false;
+    }
+     messageEl.textContent=message;
+ }
+ function newCard(){
+       
+    if(isAlive===true && hasBlackJack===false){
+        let lastCard=getRandomCard();
+        sum +=lastCard;
+        cards.push(lastCard);
+        renderGame();
+    } 
+    
+ }
+
+//cashout!
